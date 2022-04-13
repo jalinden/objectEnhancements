cb4e92a3784ab84684ba4b25277e95d128c87a49
jalinden <97492113+jalinden@users.noreply.github.com>
2022-04-12 22:18:25 -0500

    Adding completed file

---
+++ objectEnhancements.js
@@ -0,0 +1,35 @@
+//1.
+function createInstructor(firstName, lastName){
+    return {
+      firstName,
+      lastName
+    }
+  }
+
+  //2.
+let favoriteNumber = 42;
+
+const instructor = {
+    firstName: "Colt",
+    [favoriteNumber]: "That is my favorite!"
+  }
+
+  //3.
+  const instructor2 = {
+    firstName: "Colt",
+    sayHi(){
+        return"Hi!";
+    },
+    sayBye(){
+        return this.firstName + " says bye!";
+    }
+  }
+
+  //4.
+  function createAnimal(species,verb,noise) {
+      return {species,
+        [verb]() {
+            return noise;
+        } 
+      }
+  }
